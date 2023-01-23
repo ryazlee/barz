6b41b0566d6ec7e1ac4190258456ad4757632e0e
Ryan Lee <rylee@aurora.tech>
2023-01-22 21:05:02 -0800

    Migrate to typescript

--- barz_app/App.tsx
+++ barz_app/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world, this will be BARz soon :)</Text>
+      <Text style={{color:"orange", fontSize:"100px"}}>Hello world, this will be BARz soon :)</Text>
       <StatusBar style="auto" />
     </View>
   );
